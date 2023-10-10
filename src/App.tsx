8006ca4170a0dee74ab97b31551f2af0501d293f
Anguh Bleaise <bleaise.anguh.quigo@gmail.com>
2023-10-10 19:14:52 +0100

    creating app Header

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ function App() {
       {" "}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path={"/"} element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
+            <Route path={"about"} element={<About />} />
+            <Route path={"contact"} element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
